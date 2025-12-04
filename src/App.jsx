4f4b9db2647daaa0467f509ac5d1d02c9cfefd26
Praveen123-png <praveenkumar814444@gmail.com>
2025-12-04 18:57:18 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Packages from "./pages/Packages";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,10 @@
 export default function About() {
   return (
-    <>
-      <h1>About</h1>
-    </>
+    <div className="bg-gray-400 h-[1200px]">
+      {/* <h1 className="">About</h1> */}
+      <div className="flex w-full h-full justify-center items-center">
+        <img src="./images/download.jpeg" alt="" className="rounded-2xl" />
+      </div>
+    </div>
   );
 }
