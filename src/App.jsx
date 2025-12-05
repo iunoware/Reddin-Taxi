0cc1b0bab9a7f77771af8097202c436c7c13e9a7
Praveen123-png <praveenkumar814444@gmail.com>
2025-12-05 19:03:41 +0530

    navbar, hero, who we are complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,40 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Packages from "./pages/Packages";
 import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
+
+// gsap scroll smoother
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 function App() {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      wrapper: "#smoothWrapper",
+      content: "#smoothContainer",
+      smooth: 1,
+      effects: true,
+    });
+  }, []);
+
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/packages" element={<Packages />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div id="smoothWrapper">
+        <div id="smoothContainer">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/packages" element={<Packages />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
